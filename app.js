116f140e07bb072eb8ec945697a6f96b494a8cdf
Thijmen Boot <thijmenboot@outlook.com>
2018-06-22 12:14:32 +0200

    Addd http listener

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var app = express();
+const http = require('http').Server(app);
 
 var router = require("./router");
 
@@ -15,20 +16,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(router);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+http.listen(process.env.PORT || 5000, () => {
+  if (process.env.PORT !== undefined) {
+    console.log("Server gestart op poort " + process.env.PORT);
+  } else {
+    console.log("Server gestart op poort 5000");
+  }
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
